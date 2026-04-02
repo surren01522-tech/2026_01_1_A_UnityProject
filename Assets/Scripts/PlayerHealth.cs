e9b67cbcf22d08e528a88dcf7260964bebeb7366
surren01522-tech <surren01522@gmail.com>
2026-04-02 12:21:56 +0900

    [feat]유니티 프리맵 활용

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,40 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxLives = 3;
+    public int currentLives;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentLives = maxLives;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Missile"))
+        {
+            currentLives--;
+            Destroy(other.gameObject);
+
+            if(currentLives <= 0)
+            {
+                Gameover(); 
+            }
+        }
+    }
+
+    void Gameover()
+    {
+        gameObject.SetActive(false);
+        Invoke("RestarGame", 3.0f);
+    }
+
+    void RestarGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
